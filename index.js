7548fe0d6187c7e97a2c13f460f8dbec5ca9095e
GloriaSekyere <gloriasekyere15@gmail.com>
2021-12-29 08:27:19 +0000

    add error mode for invalid email

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
 let email = document.getElementById("email");
 let form = document.querySelector(".email");
+let button = document.querySelector(".email__submit");
 
 
-
-form.onsubmit = () => {
+button.onclick = (event) => {
+  event.preventDefault;
   if (email.value.match(pattern)) {
     alert("yes");
   }
   else {
-    document.querySelector(".email__submit").style.right = "-30px";
+    button.style.right = "-30px";
     document.querySelector(".validation-text").style.opacity = "1";
     email.focus();
   }
