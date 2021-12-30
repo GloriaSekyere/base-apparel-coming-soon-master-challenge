60d573063d304d9316b1b16a68ae1cc7262e0637
GloriaSekyere <gloriasekyere15@gmail.com>
2021-12-30 08:27:19 +0000

    prevent form refresh on submit

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
-let email = document.getElementById("email");
+
 let form = document.querySelector(".email");
+let email = document.querySelector(".email__field");
 let button = document.querySelector(".email__submit");
 
 
 button.onclick = (event) => {
-  event.preventDefault;
+  event.preventDefault();
   if (email.value.match(pattern)) {
     alert("yes");
   }
   else {
-    button.style.right = "-30px";
+    button.style.right = "-27px";
     document.querySelector(".validation-text").style.opacity = "1";
     email.focus();
   }
