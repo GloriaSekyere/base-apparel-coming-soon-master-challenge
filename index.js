c666ad036c810e5f83ffe053c72171490d2d6ee2
GloriaSekyere <gloriasekyere15@gmail.com>
2021-12-31 08:27:19 +0000

    complete js mobile view

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
-
 let form = document.querySelector(".email");
 let email = document.querySelector(".email__field");
 let button = document.querySelector(".email__submit");
 
-
 button.onclick = (event) => {
+  
+  // Prevent page refresh on form submission
   event.preventDefault();
+
+  // Check if email matches pattern
   if (email.value.match(pattern)) {
-    alert("yes");
+    button.style.right = "0";
+    document.querySelector(".validation-text").style.opacity = "0";
+    email.value = "";
   }
+
+  // Show error when email doesn't match pattern
   else {
     button.style.right = "-27px";
     document.querySelector(".validation-text").style.opacity = "1";
     email.focus();
   }
+  
 }
