feb77611782ce0b908f64b4f58ddf6d7689c6cad
GloriaSekyere <gloriasekyere15@gmail.com>
2022-02-01 21:36:49 +0000

    remove background code in js

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ button.onclick = (event) => {
     // Check if email matches regex pattern
     if (email.value.match(pattern)) {
       button.style.right = "0";
-      button.style.backgroundImage = "url(../images/icon-arrow.svg), \
-                                      linear-gradient(135deg,hsl(0, 80%, 86%),hsl(0, 74%, 74%))";
       error.style.right = "75px";
       validationText.style.display = "none";
       email.value = "";
@@ -29,8 +27,6 @@ button.onclick = (event) => {
     // Show error when email doesn't match pattern
     else {
       button.style.right = "-27px";
-      button.style.backgroundImage = "url(../images/icon-arrow.svg), \
-                                      linear-gradient(135deg,hsl(0, 80%, 86%),hsl(0, 80%, 86%))";
       error.style.right = "85px";
       validationText.style.display = "block";
       email.focus();
@@ -43,8 +39,6 @@ button.onclick = (event) => {
     // Check if email matches regex pattern
     if (email.value.match(pattern)) {
       button.style.right = "0";
-      button.style.backgroundImage = "url(../images/icon-arrow.svg), \
-                                      linear-gradient(135deg,hsl(0, 80%, 86%),hsl(0, 74%, 74%))";
       validationText.style.display = "none";
       email.value = "";
     }
@@ -52,8 +46,6 @@ button.onclick = (event) => {
     // Show error when email doesn't match pattern
     else {
       button.style.right = "-27px";
-      button.style.backgroundImage = "url(../images/icon-arrow.svg), \
-                                      linear-gradient(135deg,hsl(0, 80%, 86%),hsl(0, 80%, 86%))";
       validationText.style.display = "block";
       email.focus();
     }
